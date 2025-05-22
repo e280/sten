94f1360e566b78a232de2d38f2deb41a267ab690
Chase Moskal <chasemoskal@gmail.com>
2025-05-22 02:30:30 -0700

    export: colorful and colorless

--- s/index.ts
+++ s/index.ts
@@ -1,7 +1,8 @@
 
 export {Logger} from "./logger.js"
-export {Colors} from "./colors/colorful.js"
+export {colorless} from "./colors/colorless.js"
 export {Theme, asTheme} from "./themes/theme.js"
 export {Writer, asWriter} from "./writers/writer.js"
+export {Colors, colorful} from "./colors/colorful.js"
 export {Shaper, ShaperContext, ShaperFns, asShaper, combineShapers} from "./shapers/shaper.js"
 
